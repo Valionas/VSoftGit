5caafb409159b34f250a6c51cbc499a1db4183f3
Valentin <sdms@abv.bg>
2021-01-20 18:46:25 +0200

    Fieldset for better visual experience

--- vsoft/resources/views/index/index.blade.php
+++ vsoft/resources/views/index/index.blade.php
@@ -7,6 +7,17 @@
             font-style:italic;
             font-size:20px;
         }
+        fieldset{
+            border:1px solid black;
+        }
+        legend{
+            color:black;
+            font-weight:bold;
+            font-size:20px;
+        }
+        fieldset{
+            padding:5%;
+        }
 
     </style>
     <div class="inner">
@@ -16,8 +27,10 @@
             </header>
             
             @foreach($courses as $course)
-                <hr>
-                <h2>Course name: {{$course->Label}}</h2>
+                
+                <fieldset>
+                <legend>Course name: {{$course->Label}}</legend>
+               
                 <p>Starts: {{$course->Starts}}</p>
                 <p>Ends: {{$course->Ends}}</p>
                 <p>Organization: {{$course->Organization}}</p>
@@ -26,7 +39,7 @@
                     {{$lecturer->FirstName}}&nbsp;<img src="{{$lecturer->image}}" width="25px" height="25px"alt="">,
                     @endforeach</h4>
                 <h4>Location: {{$course->location->name}}</h4>
-                <hr>
+                </fieldset>
             @endforeach
             {!! $text !!}
         </div>
